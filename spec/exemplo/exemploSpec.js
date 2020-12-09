d630d5f6e79e763d778775401b7ebb4bf3af9140
joaocarlosrodriguesmaia <37455822+joaocarlosrodriguesmaia@users.noreply.github.com>
2020-12-08 23:33:38 -0300

    Arquivos do projeto local

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,11 @@
+describe("Suíte de teste de exemplo com NodeJS", function() {
+
+    it("deve demonstrar o uso do Jasmine com o NodeJS", function() {
+        expect(true).toEqual(true);
+    });
+
+    it("deve demonstrar o uso do Karma em modo de escuta", function() {
+        expect(1).toEqual(1);
+    });
+
+});
